3b510a3b9eee8bc637a209aaf36f713a2ae59d3b
Amr M. Kayid <amrkayid2027@gmail.com>
2017-11-11 17:43:28 +0200

    Finished Initial Database sql file

--- MileStone2/iWork.sql
+++ MileStone2/iWork.sql
@@ -58,62 +58,180 @@ CREATE TABLE Users (
   years_of_experience INT
 )
 
-CREATE TABLE User_prevouis_job_title (
+CREATE TABLE User_prevouis_job_titles (
   title varchar(50),
   username varchar(100) REFERENCES Users(username),
   PRIMARY KEY(title, username)
 )
    
-CREATE TABLE Applicant (
-  username varchar(100) REFERENCES Users(username)
+CREATE TABLE Applicants (
+  username varchar(100) PRIMARY KEY REFERENCES Users(username)
 )
 
-CREATE TABLE Application (
+CREATE TABLE Applications (
   id INT,
   score INT,
   hr_status varchar(50),
   job_id INT REFERENCES Jobs(job_id),
   app_username varchar(100), -- >>> APPLICATION.AppUsername references APPLICANT.username,
   hr_username varchar(100), -- >>> APPLICATION.HrUsername references HREMPLOYEE.username
-  PRIMARY KEY(ID, job_id, app_username, hr_username)
+  PRIMARY KEY(id, job_id, app_username, hr_username)
 
 )
 
 
--- ACCEPTED(ID, jobID, AppUsername, MangUsername, managerStatus) ACCEPTED.MangUsername references MANAGER.username,
--- ACCEPTED.(ID, jobID, AppUsername) references APPLICATION,
--- Constraint: ACCEPTED is an Attribute defined relation: APPLICATION.HRStatus
--- REJECTED(ID, jobID, AppUsername)
--- REJECTED.(ID, jobID, AppUsername) references APPLICATION
--- Constraint: REJECTED is an Attribute defined relation: APPLICATION.HRStatus
+CREATE TABLE Accepted (
+  manager_status varchar(50),
+  id INT REFERENCES Application(id),
+  job_id INT REFERENCES Application(job_id),
+  app_username varchar(100) REFERENCES Application(app_username), 
+  mang_username varchar(100),  -- >>> ACCEPTED.MangUsername references MANAGER.username,
+  PRIMARY KEY(id, job_id, app_username)
+) 
 
---  STAFFMEMBER(username, code, domain, companyEmail, salary, dayOff,
--- annualLeaves)
--- STAFFMEMBER.username references USER.username, STAFFMEMBER.(code, domain) references DEPARTMENT
--- A T T E N D A N C E ( username, date, timeOfStart, timeOfLeave) ATTENDANCE.username references STAFFMEMBER.username
--- E M A I L ( ID, subject, body, timeStamp)
--- SEND(ID, usernameReceiver, usernameSender) SEND.ID references EMAIL.ID
--- SEND.usernameSender references STAFFMEMBER.username, SEND.usernameReceiver references STAFFMEMBER.username
--- REQUEST(ReqNum, username, HrUsername, requestDate, startDate, endDate,
--- leaveDays, HRStatus, managerStatus)
--- REQUEST.username references STAFFMEMBER.username, REQUEST.HrUsername references HREMPLOYEE.username,
--- Where REQUEST.leaveDays = REQUEST.endDate - REQUEST.startDate
--- MANAGERREQUESTREVIEW(ReqNum, username, MangUsername, reason) MANAGERREQUESTREVIEW.(ReqNum, username) references REQUEST, MANAGERREQUESTREVIEW.MangUsername references MANAGER.username
--- BUSINESSTRIP(ReqNum, username, destination, purpose) BUSINESSTRIP.(ReqNum, username) references REQUEST
--- LEAVE(ReqNum, username, type) LEAVE.(ReqNum, username) references REQUEST
--- HREMPLOYEE(username)
--- HREMPLOYEE.username references STAFFMEMBER.username
--- REQUESTHRREPLACE(ReqNum, username, usernameReplacing) REQUESTHRREPLACE.(ReqNum, username ) references REQUEST, REQUESTHRREPLACE.usernameReplacing references HREMPLOYEE.username
--- A N N O U N C E M E N T ( ID, HrUsername, title, date, type, description) ANNOUNCEMENT.HrUsername references HREMPLOYEE.username
+CREATE TABLE Rejected (   ------- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  id INT REFERENCES Application(id),
+  job_id INT REFERENCES Application(job_id),
+  app_username varchar(100) REFERENCES Application(app_username), 
+)
 
--- MANAGER(username, type)
--- MANAGER.username references STAFFMEMBER.username
--- REQUESTMANGREPLACE(ReqNum, username, usernameReplacing) REQUESTMANGREPLACE.(ReqNum, username ) references REQUEST, REQUESTMANGREPLACE.usernameReplacing references MANAGER.username
--- R E G U L A R E M P L O Y E E ( username) REGULAREMPLOYEE.username references STAFFMEMBER.username
--- REQUESTREGEREPLACE(ReqNum, username, usernameReplacing) REQUESTREGEREPLACE.(ReqNum, username) references REQUEST, REQUESTREGEREPLACE.usernameReplacing references REGULAREMPLOYEE.username
--- PROJECT(name, MangDefineUsername, startDate, endDate) PROJECT.MangDefineUsername references MANAGER.username
--- PROJECTASSIGNMENT(projName, RegEUsername, MangUsername) PROJECTASSIGNMENT.projName references PROJECT.name, PROJECTASSIGNMENT.MangUsername references MANAGER.username, PROJECTASSIGNMENT.RegEUsername references REGULAREMPLOYEE.username
--- T A S K ( name, projectName, RegEUsername, MangUsername, description, status,
--- deadline)
--- TASK.projectName references PROJECT.name, TASK.MangUsername references MANAGER.username, TASK.RegEUsername references REGULAREMPLOYEE.username
--- COMMENT(ID, name, projectName, username, content) COMMENT.(name, projectName) references TASK, COMMENT.username references STAFFMEMBER.username
+CREATE TABLE Staff_Members (
+  username varchar(100) PRIMARY KEY REFERENCES Users(username),
+  -- code,    <<<<<<<<< Change with job_id ???????!!!!
+  -- domain,
+  company_email varchar(50),
+  salary DECIMAL,
+  day_off DATETIME,
+  annual_leaves INT
+)
+
+CREATE TABLE Attendances (
+  date DATETIME,
+  username varchar(100)  REFERENCES Staff_Member(username),  
+  time_of_start TIME, ------- NOT SURE
+  time_of_leave TIME,
+  PRIMARY KEY(username, date)
+)
+CREATE TABLE Emails (
+  id INT PRIMARY KEY,
+  subject varchar(50),
+  body varchar(max),
+  time_stamp TIMESTAMP
+)
+CREATE TABLE Sends (
+  id INT,
+  username_receiver varchar(100)  REFERENCES Staff_Member(username),
+  username_sender varchar(100)  REFERENCES Staff_Member(username)
+  PRIMARY KEY(id, username_receiver)
+)
+    
+CREATE TABLE Requests (
+  req_num INT,
+  request_date DATETIME,
+  start_date DATETIME,
+  end_date DATETIME,
+  leave_days AS (start_date - end_date),
+  hr_status varchar(50),
+  manager_status varchar(50),
+  username  varchar(100)  REFERENCES Staff_Member(username),
+  hr_username varchar(100), -- >>>> REQUEST.HrUsername references HREMPLOYEE.username,
+  PRIMARY KEY(req_num, username)
+)
+
+CREATE TABLE Manager_Request_Reviews (
+  req_num INT  REFERENCES Request(req_num),
+  username  varchar(100)  REFERENCES Request(username),
+  reason varchar(100),
+  mang_username varchar(100),  -- >>> MANAGERREQUESTREVIEW.MangUsername references MANAGER.username
+  PRIMARY KEY(req_num, username)
+) 
+CREATE TABLE Business_Trips (
+  req_num INT REFERENCES Request(req_num),
+  username  varchar(100)  REFERENCES Request(username),
+  destination varchar(100),
+  purpose varchar(100),
+  PRIMARY KEY(req_num, username)
+)
+
+CREATE TABLE Leaves (
+  req_num INT REFERENCES Request(req_num),
+  username  varchar(100)  REFERENCES Request(username),
+  type varchar(100),
+  PRIMARY KEY(req_num, username)
+)
+
+CREATE TABLE Hr_Employees (
+  username varchar(100) PRIMARY KEY REFERENCES Staff_Member(username)
+)
+
+CREATE TABLE Request_Hr_Replace (
+  req_num INT REFERENCES Request(req_num),
+  username  varchar(100)  REFERENCES Request(username),
+  username_replacing varchar(100) REFERENCES Hr_Employees(username)
+  PRIMARY KEY(req_num, username)
+) 
+
+CREATE TABLE Announcements ( 
+  id INT PRIMARY KEY,
+  title varchar(50),
+  date DATETIME,
+  type varchar(50),
+  description varchar(max),
+  hr_username varchar(100) REFERENCES Hr_Employees(username)
+)
+
+CREATE TABLE Managers (
+  username varchar(100) PRIMARY KEY REFERENCES Staff_Member(username), 
+  type varchar(100)
+)
+
+CREATE TABLE Request_Mang_Replace (
+  req_num INT REFERENCES Request(req_num),
+  username  varchar(100)  REFERENCES Request(username),
+  username_replacing varchar(100) REFERENCES Managers(username)
+  PRIMARY KEY(req_num, username)
+)
+
+CREATE TABLE Regular_Employees (
+  username varchar(100) PRIMARY KEY REFERENCES Staff_Member(username)
+)
+
+CREATE TABLE Request_RegE_Replace (
+  req_num INT REFERENCES Request(req_num),
+  username  varchar(100)  REFERENCES Request(username),
+  username_replacing varchar(100) REFERENCES Regular_Employees(username)
+  PRIMARY KEY(req_num, username)
+)
+
+CREATE TABLE Projects (
+  name varchar(50), ---- <<<<<<< PRIMARY KEY MODIFYYYYYY
+  mang_define_username varchar(100) REFERENCES Managers(username),
+  start_date DATETIME,
+  end_date DATETIME
+)
+
+CREATE TABLE Project_Assignments (
+  project_name varchar(50) REFERENCES Projects(name),
+  regE_username varchar(100) REFERENCES Regular_Employees(username),
+  mang_username varchar(100) REFERENCES Managers(username),
+  PRIMARY KEY(project_name, regE_username) ---- <<<<<<< PRIMARY KEY MODIFYYYYYY
+) 
+CREATE TABLE Tasks (
+  name varchar(50),
+  description varchar(max),
+  status varchar(50),
+  deadline DATETIME,
+  project_name varchar(50) REFERENCES Projects(name),
+  regE_username varchar(100) REFERENCES Regular_Employees(username),
+  mang_username varchar(100) REFERENCES Managers(username),
+  PRIMARY KEY(name, project_name) ---- <<<<<<< PRIMARY KEY MODIFYYYYYY
+)
+
+CREATE TABLE Comments (
+  id INT,
+  content varchar(50),
+  name varchar(50) REFERENCES Tasks(name),
+  project_name varchar(50) REFERENCES Tasks(project_name),
+  username varchar(100) REFERENCES Staff_Member(username),
+  PRIMARY KEY(id, name, project_name) ---- <<<<<<< PRIMARY KEY MODIFYYYYYY
+) 
